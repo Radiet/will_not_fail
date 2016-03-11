3d8684e4ce93f83de40c292a184de4cc75b2e600
radiet <radiet@41studio.com>
2016-03-12 04:58:52 +0700

    change movement default

--- app/assets/javascripts/fields.js
+++ app/assets/javascripts/fields.js
@@ -13,17 +13,26 @@ $(document).ready(function() {
 
   iso = Crafty.isometric.init(128);
   var z = 0;
+  var charSelected = false
+  var currentChar
+
   for(var i = 20; i >= 0; i--) {
     for(var y = 0; y < 20; y++) {
       var which = Crafty.randRange(0,1);
       var tile = Crafty.e("2D, DOM, "+ (!which ? "stone" : "grass") +", Mouse")
       .attr('z',i+1 * y+1).areaMap([64,0],[128,32],[128,96],[64,128],[0,96],[0,32]).bind("click", function(e) {
         //destroy on right click
-        if(e.button === 2) this.destroy();
+        if(e.button === 0){
+          if(charSelected){
+            getDirection(this.attr('y'), this.attr('x'))
+          }
+        }
       }).bind("mouseover", function() {
         if(this.has("grass")) {
+          console.log(this.attr('y'), this.attr('x'))
           this.sprite(0,1,1,1);
         } else {
+          console.log(this.attr('y'), this.attr('x'))
           this.sprite(1,1,1,1);
         }
       }).bind("mouseout", function() {
@@ -38,88 +47,59 @@ $(document).ready(function() {
     }
   }
 
-  var man = Crafty.e("2D, DOM, hoodF, Mouse")
+  window.man = Crafty.e("2D, DOM, hoodF, Mouse")
+              .bind("click", function(e) {
+                if(e.button === 0){
+                  charSelected = true
+                  currentChar = this
+                }
+              })
+              .bind("mouseover", function() {
+                  console.log(this.attr('y'), this.attr('x'))
+              })
   iso.place(0,0.5,2, man);
   man.attr('y', 230).attr('x', 675).attr('z', 10000)
-  // man.attr('y', -25).attr('x', 25).attr('z', 15)
-  // man.attr('y', -125).attr('x', 90).attr('z', 15) left first
-  // man.attr('y', 70).attr('x', 350).attr('z', 15)
 
-  var frontMan = Crafty.e("2D, DOM, hoodF, Mouse")
-    .bind("click", function(e) {
-        if(e.button === 0){
-          moveToFront('front', spriteMan)
-          console.log(man)
-          man.sprite(7,0,0,1.5);
-        }
-      })
-
-  iso.place(0,0.5,2, frontMan);
-  frontMan.attr('y', man.attr('y')+32).attr('x', man.attr('x')+65).attr('z', 10000)
-
-  var backMan = Crafty.e("2D, DOM, hoodB, Mouse")
-    .bind("click", function(e) {
-        if(e.button === 0){
-          moveToFront('back', spriteMan)
-          console.log(man)
-          man.sprite(3,0,0,1.5);
-        }
-      })
-
-  iso.place(0,0.5,2, backMan);
-  backMan.attr('y', man.attr('y')-32).attr('x', man.attr('x')-65).attr('z', 10000)
-
-  var leftMan = Crafty.e("2D, DOM, hoodR, Mouse")
-    .bind("click", function(e) {
-        if(e.button === 0){
-          moveToFront('right', spriteMan)
-          console.log(man)
-          man.sprite(5,0,0,1.5);
-        }
-      })
-
-  iso.place(0,0.5,2, leftMan);
-  leftMan.attr('y', man.attr('y')-32).attr('x', man.attr('x')+65).attr('z', 9000)
-
-  var rightMan = Crafty.e("2D, DOM, hoodL, Mouse")
-    .bind("click", function(e) {
-        if(e.button === 0){
-          moveToFront('left', spriteMan)
-          console.log(man)
-          man.sprite(1,0,0,1.5);
-        }
-      })
-
-  iso.place(0,0.5,2, rightMan);
-  rightMan.attr('y', man.attr('y')+32).attr('x', man.attr('x')-65).attr('z', 10000)
-
-  var spriteMan = [leftMan, man, frontMan, backMan, rightMan]
-
-  function moveToFront(direction, element){
+  function moveTo(direction, element){
     switch(direction){
       case 'front':
         incY = 32
-        incX = 65
+        incX = 64
+        element.sprite(7,0,0,1.5);
         break
       case 'back':
         incY = -32
-        incX = -65
+        incX = -64
+        element.sprite(3,0,0,1.5);
         break
       case 'left':
         incY = 32
-        incX = -65
+        element.sprite(1,0,0,1.5);
+        incX = -64
         break
       case 'right':
         incY = -32
+        element.sprite(5,0,0,1.5);
         incX = 65
         break
     }
 
-    element.forEach(function(elem, i){
-      var y = elem.attr('y') + incY
-      var x = elem.attr('x') + incX
-      elem.attr('y', y).attr('x', x).attr('z', 10000+ i)
-    })
+    var y = element.attr('y') + incY
+    var x = element.attr('x') + incX
+    element.attr('y', y).attr('x', x).attr('z', 10000+ i)
+  }
+
+  function getDirection(y, x) {
+    window.fromY = currentChar.attr('y') + 90
+    window.fromX = currentChar.attr('x') - 35
+    if(fromY<y && fromX<x) moveTo('front', currentChar)
+    if(fromY>y && fromX<x) moveTo('right', currentChar)
+    if(fromY<y && fromX>x) moveTo('left', currentChar)
+    if(fromY>y && fromX>x) moveTo('back', currentChar)
+    // 352 704
+    // 320 640
+    // 230 675
+    // 90  35
   }
 
   Crafty.addEvent(this, Crafty.stage.elem, "mousedown", function(e) {
